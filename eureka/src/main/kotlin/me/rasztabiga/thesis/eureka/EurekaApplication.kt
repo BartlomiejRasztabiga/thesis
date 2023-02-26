eed58f93b0c0d033baa003656882e69f42625a8a
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-02-26 17:39:29 +0100

    eureka: Initial commit

--- eureka/src/main/kotlin/me/rasztabiga/thesis/eureka/EurekaApplication.kt
+++ eureka/src/main/kotlin/me/rasztabiga/thesis/eureka/EurekaApplication.kt
@@ -2,8 +2,10 @@ package me.rasztabiga.thesis.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaApplication
 
 fun main(args: Array<String>) {
