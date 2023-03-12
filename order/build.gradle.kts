8aa709990f3d3a96ac8354b377fb8c453a8a513a
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-03-12 19:55:37 +0100

    Add order service

---
+++ order/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.9"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("org.jetbrains.kotlin.jvm") version "1.8.10"
+	id("org.jetbrains.kotlin.plugin.spring") version "1.8.10"
+}
+
+group = "me.rasztabiga.thesis"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+	maven {
+		url = uri("https://maven.pkg.github.com/bartlomiejrasztabiga/thesis")
+		credentials {
+			username = project.property("GITHUB_ACTOR").toString()
+			password = project.property("GITHUB_TOKEN").toString()
+		}
+	}
+}
+
+val axonVersion = "4.7.2"
+val testcontainersVersion = "1.17.6"
+
+dependencies {
+	implementation("me.rasztabiga.thesis:shared:sha-e8420d8")
+
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.axonframework:axon-spring-boot-starter")
+	implementation("org.axonframework.extensions.kotlin:axon-kotlin")
+	implementation(kotlin("reflect"))
+	implementation(kotlin("stdlib"))
+	runtimeOnly("org.springframework.boot:spring-boot-devtools")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.axonframework:axon-test")
+	testImplementation("org.testcontainers:junit-jupiter")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.axonframework:axon-bom:${axonVersion}")
+		mavenBom("org.testcontainers:testcontainers-bom:${testcontainersVersion}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.getByName<Jar>("jar") {
+	enabled = false
+}
---
+++ order/src/test/kotlin/me/rasztabiga/thesis/order/OrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.rasztabiga.thesis.order
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
