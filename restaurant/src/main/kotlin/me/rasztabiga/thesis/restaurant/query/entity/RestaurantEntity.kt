1dbafe7fb1539f3a631f44d16a7ca4d37861095f
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-04-01 18:46:12 +0200

    Add restaurant create & read

--- restaurant/src/main/kotlin/me/rasztabiga/thesis/restaurant/query/entity/RestaurantEntity.kt
+++ restaurant/src/main/kotlin/me/rasztabiga/thesis/restaurant/query/entity/RestaurantEntity.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.UUID
 
-@Document
+@Document(collection = "restaurant")
 data class RestaurantEntity(
     @Id
     val id: UUID,
