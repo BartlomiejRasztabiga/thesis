f89bf5f57becee82917e580eb69a70117f89293c
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-05-03 20:08:17 +0200

    ci(backend) Upgrade dependencies

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.6"
 	id("io.spring.dependency-management") version "1.1.0"
-	id("org.jetbrains.kotlin.jvm") version "1.8.20"
-	id("org.jetbrains.kotlin.plugin.spring") version "1.8.20"
+	id("org.jetbrains.kotlin.jvm") version "1.8.21"
+	id("org.jetbrains.kotlin.plugin.spring") version "1.8.21"
 	id("io.gitlab.arturbosch.detekt") version "1.23.0-RC3"
 }
 
