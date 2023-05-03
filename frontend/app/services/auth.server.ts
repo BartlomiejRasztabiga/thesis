ba9d6063b23ca564f61f55c4c84f1b857f98fcac
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-05-03 19:55:30 +0200

    feat(frontend): Basic auth

--- frontend/app/services/auth.server.ts
+++ frontend/app/services/auth.server.ts
@@ -2,10 +2,11 @@ import { Authenticator } from "remix-auth";
 import { Auth0Strategy } from "remix-auth-auth0";
 
 import {sessionStorage} from "~/services/session.server";
+import invariant from "tiny-invariant";
 
-interface User {
-    email: string
-}
+invariant(process.env.AUTH_CALLBACK_URL, "AUTH_CALLBACK_URL must be set");
+invariant(process.env.AUTH_CLIENT_ID, "AUTH_CLIENT_ID must be set");
+invariant(process.env.AUTH_CLIENT_SECRET, "AUTH_CLIENT_SECRET must be set");
 
 // Create an instance of the authenticator, pass a generic with what your
 // strategies will return and will be stored in the session
@@ -13,9 +14,9 @@ export const authenticator = new Authenticator<any>(sessionStorage);
 
 let auth0Strategy = new Auth0Strategy(
     {
-        callbackURL: "http://localhost:3000/auth/auth0/callback",
-        clientID: "3gSK8og83MMTCUlNrzGNfnzpBNyLlpTS",
-        clientSecret: "fxntskIoYgCCXgxT-DYzTZraK4OwX-mwAcv8C4kZdszLDedqdtuwgTK8xB840C2G",
+        callbackURL: process.env.AUTH_CALLBACK_URL,
+        clientID: process.env.AUTH_CLIENT_ID,
+        clientSecret: process.env.AUTH_CLIENT_SECRET,
         domain: "rasztabigab.eu.auth0.com",
         scope: "openid profile email read:restaurants write:restaurants read:users write:users",
         audience: "https://thesis.rasztabiga.me/api"
