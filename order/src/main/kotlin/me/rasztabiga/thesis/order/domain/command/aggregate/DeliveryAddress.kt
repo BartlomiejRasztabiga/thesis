e6dd0c335b70774f89725889d29b784e42fc0dcd
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-05-17 19:58:52 +0200

    feat(order): WIP Get order by id + tests

--- order/src/main/kotlin/me/rasztabiga/thesis/order/domain/command/aggregate/DeliveryAddress.kt
+++ order/src/main/kotlin/me/rasztabiga/thesis/order/domain/command/aggregate/DeliveryAddress.kt
@@ -1,18 +1,15 @@
 package me.rasztabiga.thesis.order.domain.command.aggregate
 
-import me.rasztabiga.thesis.order.domain.command.command.DeleteDeliveryAddressCommand
-import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.modelling.command.EntityId
 import java.util.*
-import org.axonframework.modelling.command.AggregateLifecycle.apply
 
 internal class DeliveryAddress {
 
     @EntityId
-    lateinit var addressId: UUID
+    var addressId: UUID
         private set
 
-    private lateinit var address: String
+    private var address: String
     private var additionalInfo: String? = null
 
     constructor(addressId: UUID, address: String, additionalInfo: String?) {
