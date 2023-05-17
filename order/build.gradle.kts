82e0dd899aab546d636d7509a4d19df7b81451ab
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-05-17 19:59:45 +0200

    ci(backend) Upgrade dependencies

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -27,8 +27,8 @@ repositories {
 }
 
 val axonVersion = "4.7.3"
-val testcontainersVersion = "1.18.0"
-val kotestVersion = "5.6.1"
+val testcontainersVersion = "1.18.1"
+val kotestVersion = "5.6.2"
 
 dependencies {
     implementation("me.rasztabiga.thesis:shared:0.6.1")
