2c40d333a9b91de438da2535817d3912861edc79
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-07-23 00:27:23 +0200

    ci(frontend): Upgrade dependencies

--- frontend/cypress/e2e/smoke.cy.ts
+++ frontend/cypress/e2e/smoke.cy.ts
@@ -1,20 +1,6 @@
 describe("smoke tests", () => {
   afterEach(() => {});
 
-  it("should allow you to make a note", () => {
-    cy.visitAndCheck("/");
-
-    // cy.findByRole("link", { name: /notes/i }).click();
-    // cy.findByText("No notes yet");
-    //
-    // cy.findByRole("link", { name: /\+ new note/i }).click();
-    //
-    // cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    // cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    // cy.findByRole("button", { name: /save/i }).click();
-    //
-    // cy.findByRole("button", { name: /delete/i }).click();
-    //
-    // cy.findByText("No notes yet");
+  it("should allow you to start an order", () => {
   });
 });
