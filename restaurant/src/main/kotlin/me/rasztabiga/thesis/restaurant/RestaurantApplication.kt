4b08e3edace3f2f704fb26d201262531b37d28da
Bartłomiej Rasztabiga <bartlomiej.rasztabiga@symmetrical.ai>
2023-07-22 17:36:00 +0200

    feat: WIP Move common classes to shared

--- restaurant/src/main/kotlin/me/rasztabiga/thesis/restaurant/RestaurantApplication.kt
+++ restaurant/src/main/kotlin/me/rasztabiga/thesis/restaurant/RestaurantApplication.kt
@@ -1,9 +1,13 @@
 package me.rasztabiga.thesis.restaurant
 
+import me.rasztabiga.thesis.shared.config.GlobalErrorWebExceptionHandler
+import me.rasztabiga.thesis.shared.security.SecurityConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(SecurityConfig::class, GlobalErrorWebExceptionHandler::class)
 class RestaurantApplication
 
 @Suppress("SpreadOperator")
