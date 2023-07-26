01c60785fda1ee9e4fdbe2ef8e00930be1c0cf11
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-07-26 14:03:16 +0200

    feat(frontend): WIP Add & remove order item

--- frontend/app/services/session.server.ts
+++ frontend/app/services/session.server.ts
@@ -6,11 +6,11 @@ invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__session",
-    // httpOnly: true,
-    // path: "/",
+    httpOnly: true,
+    path: "/",
     sameSite: "lax",
     secrets: [process.env.SESSION_SECRET],
-    // secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production",
   },
 });
 
