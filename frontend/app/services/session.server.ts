0e9389002529dfb91483755f4e15df4f9328c853
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-07-26 11:53:01 +0200

    feat(frontend): WIP Add & remove order item

--- frontend/app/services/session.server.ts
+++ frontend/app/services/session.server.ts
@@ -10,7 +10,7 @@ export const sessionStorage = createCookieSessionStorage({
     path: "/",
     sameSite: "lax",
     secrets: [process.env.SESSION_SECRET],
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
   },
 });
 
