90286cdbd83c291695caee2d83ec9c68008d7b3c
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-08-05 19:18:55 +0200

    feat(order): WIP Finalize order

---
+++ shared/src/main/kotlin/me/rasztabiga/thesis/order/domain/command/command/CalculateOrderTotalCommand.kt
@@ -0,0 +1,15 @@
+package me.rasztabiga.thesis.order.domain.command.command
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier
+import java.util.*
+
+data class CalculateOrderTotalCommand(
+    val orderId: UUID,
+    @TargetAggregateIdentifier val restaurantId: UUID,
+    val items: List<OrderItem>
+) {
+    data class OrderItem(
+        val orderItemId: UUID,
+        val productId: UUID
+    )
+}
