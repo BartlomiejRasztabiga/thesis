a3c1423eafa6df8c8c8fa8e37d67a1af6d45a38d
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-08-12 16:43:45 +0200

    feat(restaurant): WIP Order confirmed by restaurant

--- shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/command/CreateRestaurantOrderCommand.kt
+++ shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/command/CreateRestaurantOrderCommand.kt
@@ -1,8 +1,14 @@
 package me.rasztabiga.thesis.shared.domain.command.command
 
 import org.axonframework.modelling.command.TargetAggregateIdentifier
-import java.util.UUID
+import java.util.*
 
 data class CreateRestaurantOrderCommand(
-    @TargetAggregateIdentifier val orderId: UUID
-)
+    @TargetAggregateIdentifier val orderId: UUID,
+    val items: List<OrderItem>
+) {
+    data class OrderItem(
+        val productId: UUID
+    )
+}
+
