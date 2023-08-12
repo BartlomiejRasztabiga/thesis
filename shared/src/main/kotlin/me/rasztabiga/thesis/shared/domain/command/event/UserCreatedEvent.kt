2eecade52cebb09bbf100a40cb8009e2bcec8bed
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-08-12 17:54:13 +0200

    feat(restaurant): WIP Order confirmed by restaurant

--- shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/event/UserCreatedEvent.kt
+++ shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/event/UserCreatedEvent.kt
@@ -2,7 +2,7 @@ package me.rasztabiga.thesis.shared.domain.command.event
 
 import org.axonframework.serialization.Revision
 
-@Revision("1.0")
+@Revision("2.0")
 data class UserCreatedEvent(
     val userId: String,
     val name: String
