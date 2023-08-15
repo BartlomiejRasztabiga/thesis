df4ffeb8af5ea1a56d29a6fc4f7219ac109b2151
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-08-15 12:06:24 +0200

    feat(delivery): WIP Courier created

--- shared/src/main/kotlin/me/rasztabiga/thesis/shared/security/Scopes.kt
+++ shared/src/main/kotlin/me/rasztabiga/thesis/shared/security/Scopes.kt
@@ -5,6 +5,7 @@ object Scopes {
     val RESTAURANT = RestaurantScopes
     val USER = UserScopes
     val ORDER = OrderScopes
+    val DELIVERY = DeliveryScopes
 
     object RestaurantScopes {
         private const val RESTAURANTS = "restaurants"
@@ -25,6 +26,13 @@ object Scopes {
 
         const val READ = "${SCOPE}_read:${ORDERS}"
         const val WRITE = "${SCOPE}_write:${ORDERS}"
+    }
+
+    object DeliveryScopes {
+        private const val DELIVERIES = "deliveries"
+
+        const val READ = "${SCOPE}_read:${DELIVERIES}"
+        const val WRITE = "${SCOPE}_write:${DELIVERIES}"
     }
 
     private const val SCOPE = "SCOPE"
