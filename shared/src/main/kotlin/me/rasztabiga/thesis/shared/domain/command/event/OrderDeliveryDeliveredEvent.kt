5a9d3e56d9fdaad64564e138db4cd3ccd7123fbc
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-08-19 17:32:27 +0200

    feat(delivery): Delivery offer rejected

--- shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/event/OrderDeliveryDeliveredEvent.kt
+++ shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/event/OrderDeliveryDeliveredEvent.kt
@@ -1,11 +1,11 @@
-package me.rasztabiga.thesis.delivery.domain.command.event
+package me.rasztabiga.thesis.shared.domain.command.event
 
 import org.axonframework.serialization.Revision
 import java.math.BigDecimal
 import java.util.*
 
 @Revision("1.0")
-data class OrderDeliveryPickedUpEvent(
+data class OrderDeliveryDeliveredEvent(
     val deliveryId: UUID,
     val courierId: String
 )
--- shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/event/OrderDeliveryPickedUpEvent.kt
+++ shared/src/main/kotlin/me/rasztabiga/thesis/shared/domain/command/event/OrderDeliveryPickedUpEvent.kt
@@ -1,11 +1,11 @@
-package me.rasztabiga.thesis.delivery.domain.command.event
+package me.rasztabiga.thesis.shared.domain.command.event
 
 import org.axonframework.serialization.Revision
 import java.math.BigDecimal
 import java.util.*
 
 @Revision("1.0")
-data class OrderDeliveryDeliveredEvent(
+data class OrderDeliveryPickedUpEvent(
     val deliveryId: UUID,
     val courierId: String
 )
