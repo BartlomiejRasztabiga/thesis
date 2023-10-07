e70ac83b63cbd44ab06da9a4a3183de14fecca7c
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-10-07 20:30:28 +0200

    feat(restaurant): WIP Make user pay for delivery

--- restaurant/src/main/kotlin/me/rasztabiga/thesis/payment/adapter/in/rest/api/UpdateRestaurantMenuRequest.kt
+++ restaurant/src/main/kotlin/me/rasztabiga/thesis/payment/adapter/in/rest/api/UpdateRestaurantMenuRequest.kt
@@ -2,12 +2,14 @@
 
 package me.rasztabiga.thesis.payment.adapter.`in`.rest.api
 
+import java.math.BigDecimal
+
 data class UpdateRestaurantMenuRequest(
     val menu: List<Product>
 ) {
     data class Product(
         val name: String,
         val description: String?,
-        val price: Double,
+        val price: BigDecimal,
     )
 }
