b17cd54654fc818db2dcee647912f8bd8d6c7a98
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-10-15 16:21:24 +0200

    feat(frontend): WIP UI overhaul

--- frontend/app/routes/v2.ordering.restaurants.tsx
+++ frontend/app/routes/v2.ordering.restaurants.tsx
@@ -3,21 +3,9 @@ import { json } from "@remix-run/node";
 import { getRestaurants } from "~/models/restaurant.server";
 import BottomNavbar from "~/components/BottomNavbar";
 import { useLoaderData } from "@remix-run/react";
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
 import { CardActionArea } from "@mui/material";
 
 export async function loader({ request }: LoaderArgs) {
@@ -33,25 +21,28 @@ export default function V2RestaurantsPage() {
       <div className="h-full">
         <div className="flex flex-col w-80 mx-auto">
           {data.restaurants.map((restaurant, key) => (
-            <Card key={key} className={"my-4"} style={{minWidth: 500}}>
+            <Card key={key} className={"my-4"} style={{ minWidth: 500 }}>
               <CardActionArea>
-              <CardMedia
-                component="img"
-                height="194"
-                image={restaurant.imageUrl}
-              />
-              <CardContent>
-                <div className="flex flex-row w-full">
-                  <div className="flex-col w-full">
-                    <h5 className="text-lg font-bold">{restaurant.name}</h5>
-                    <p>delivery fee</p>
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={restaurant.imageUrl}
+                />
+                <CardContent>
+                  <div className="flex flex-row w-full">
+                    <div className="flex-col w-full">
+                      <h5 className="text-lg font-bold">{restaurant.name}</h5>
+                      <p>delivery fee</p>
+                    </div>
+                    <div
+                      className="flex-col w-full"
+                      style={{ textAlign: "right" }}
+                    >
+                      <h5>&nbsp;</h5>
+                      <p>rating</p>
+                    </div>
                   </div>
-                  <div className="flex-col w-full" style={{textAlign: "right"}}>
-                    <h5>&nbsp;</h5>
-                    <p>rating</p>
-                  </div>
-                </div>
-              </CardContent>
+                </CardContent>
               </CardActionArea>
             </Card>
           ))}
