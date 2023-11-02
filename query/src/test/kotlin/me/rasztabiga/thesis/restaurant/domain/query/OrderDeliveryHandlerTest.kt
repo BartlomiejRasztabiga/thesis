c3f08090f00739155460e505615edb473772f3c0
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-11-02 20:14:43 +0100

    feat(backend): WIP unit tests

--- query/src/test/kotlin/me/rasztabiga/thesis/restaurant/domain/query/OrderDeliveryHandlerTest.kt
+++ query/src/test/kotlin/me/rasztabiga/thesis/restaurant/domain/query/OrderDeliveryHandlerTest.kt
@@ -1,5 +1,6 @@
 package me.rasztabiga.thesis.restaurant.domain.query
 
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.mockk.mockk
@@ -9,7 +10,10 @@ import me.rasztabiga.thesis.restaurant.domain.query.repository.InMemoryCourierRe
 import me.rasztabiga.thesis.restaurant.domain.query.repository.InMemoryOrderDeliveryRepository
 import me.rasztabiga.thesis.shared.adapter.`in`.rest.api.Location
 import me.rasztabiga.thesis.shared.adapter.`in`.rest.api.OrderDeliveryResponse
+import me.rasztabiga.thesis.shared.domain.command.event.OrderDeliveryAcceptedEvent
 import me.rasztabiga.thesis.shared.domain.command.event.OrderDeliveryCreatedEvent
+import me.rasztabiga.thesis.shared.domain.command.event.OrderDeliveryDeliveredEvent
+import me.rasztabiga.thesis.shared.domain.command.event.OrderDeliveryPickedUpEvent
 import me.rasztabiga.thesis.shared.domain.command.event.OrderDeliveryRejectedEvent
 import me.rasztabiga.thesis.shared.domain.query.query.FindOrderDeliveryByIdQuery
 import org.junit.jupiter.api.BeforeEach
@@ -52,6 +56,7 @@ class OrderDeliveryHandlerTest {
         savedOrderDelivery?.status shouldBe OrderDeliveryResponse.DeliveryStatus.OFFER
         savedOrderDelivery?.courierFee shouldBe event.courierFee
         savedOrderDelivery?.createdAt shouldNotBe null
+        savedOrderDelivery?.courierIdsDeclined?.size shouldBe 0
     }
 
     @Test
@@ -81,5 +86,120 @@ class OrderDeliveryHandlerTest {
         savedOrderDelivery?.status shouldBe OrderDeliveryResponse.DeliveryStatus.OFFER
         savedOrderDelivery?.courierFee shouldBe orderDeliveryCreatedEvent.courierFee
         savedOrderDelivery?.createdAt shouldNotBe null
+        savedOrderDelivery?.courierIdsDeclined?.shouldContain(orderDeliveryRejectedEvent.courierId)
+    }
+
+    @Test
+    fun `should handle OrderDeliveryAcceptedEvent`() {
+        val orderDeliveryCreatedEvent = OrderDeliveryCreatedEvent(
+            deliveryId = UUID.randomUUID(),
+            orderId = UUID.randomUUID(),
+            restaurantLocation = Location(1.0, 1.0, null),
+            deliveryLocation = Location(2.0, 2.0, null),
+            courierFee = 1.toBigDecimal(),
+        )
+
+        val orderDeliveryAcceptedEvent = OrderDeliveryAcceptedEvent(
+            deliveryId = orderDeliveryCreatedEvent.deliveryId,
+            orderId = orderDeliveryCreatedEvent.orderId,
+            courierId = UUID.randomUUID().toString()
+        )
+
+        orderDeliveryHandler.on(orderDeliveryCreatedEvent)
+        orderDeliveryHandler.on(orderDeliveryAcceptedEvent)
+
+        val savedOrderDelivery =
+            orderDeliveryHandler.handle(FindOrderDeliveryByIdQuery(orderDeliveryAcceptedEvent.deliveryId)).block()
+        savedOrderDelivery shouldNotBe null
+        savedOrderDelivery?.id shouldBe orderDeliveryCreatedEvent.deliveryId
+        savedOrderDelivery?.restaurantLocation shouldBe orderDeliveryCreatedEvent.restaurantLocation
+        savedOrderDelivery?.deliveryLocation shouldBe orderDeliveryCreatedEvent.deliveryLocation
+        savedOrderDelivery?.status shouldBe OrderDeliveryResponse.DeliveryStatus.ACCEPTED
+        savedOrderDelivery?.courierFee shouldBe orderDeliveryCreatedEvent.courierFee
+        savedOrderDelivery?.createdAt shouldNotBe null
+        savedOrderDelivery?.courierIdsDeclined?.size shouldBe 0
+    }
+
+    @Test
+    fun `should handle OrderDeliveryPickedUpEvent`() {
+        val orderDeliveryCreatedEvent = OrderDeliveryCreatedEvent(
+            deliveryId = UUID.randomUUID(),
+            orderId = UUID.randomUUID(),
+            restaurantLocation = Location(1.0, 1.0, null),
+            deliveryLocation = Location(2.0, 2.0, null),
+            courierFee = 1.toBigDecimal(),
+        )
+
+        val orderDeliveryAcceptedEvent = OrderDeliveryAcceptedEvent(
+            deliveryId = orderDeliveryCreatedEvent.deliveryId,
+            orderId = orderDeliveryCreatedEvent.orderId,
+            courierId = UUID.randomUUID().toString()
+        )
+
+        val orderDeliveryPickedUpEvent = OrderDeliveryPickedUpEvent(
+            deliveryId = orderDeliveryCreatedEvent.deliveryId,
+            orderId = orderDeliveryCreatedEvent.orderId,
+            courierId = UUID.randomUUID().toString()
+        )
+
+        orderDeliveryHandler.on(orderDeliveryCreatedEvent)
+        orderDeliveryHandler.on(orderDeliveryAcceptedEvent)
+        orderDeliveryHandler.on(orderDeliveryPickedUpEvent)
+
+        val savedOrderDelivery =
+            orderDeliveryHandler.handle(FindOrderDeliveryByIdQuery(orderDeliveryAcceptedEvent.deliveryId)).block()
+        savedOrderDelivery shouldNotBe null
+        savedOrderDelivery?.id shouldBe orderDeliveryCreatedEvent.deliveryId
+        savedOrderDelivery?.restaurantLocation shouldBe orderDeliveryCreatedEvent.restaurantLocation
+        savedOrderDelivery?.deliveryLocation shouldBe orderDeliveryCreatedEvent.deliveryLocation
+        savedOrderDelivery?.status shouldBe OrderDeliveryResponse.DeliveryStatus.PICKED_UP
+        savedOrderDelivery?.courierFee shouldBe orderDeliveryCreatedEvent.courierFee
+        savedOrderDelivery?.createdAt shouldNotBe null
+        savedOrderDelivery?.courierIdsDeclined?.size shouldBe 0
+    }
+
+    @Test
+    fun `should handle OrderDeliveryDeliveredEvent`() {
+        val orderDeliveryCreatedEvent = OrderDeliveryCreatedEvent(
+            deliveryId = UUID.randomUUID(),
+            orderId = UUID.randomUUID(),
+            restaurantLocation = Location(1.0, 1.0, null),
+            deliveryLocation = Location(2.0, 2.0, null),
+            courierFee = 1.toBigDecimal(),
+        )
+
+        val orderDeliveryAcceptedEvent = OrderDeliveryAcceptedEvent(
+            deliveryId = orderDeliveryCreatedEvent.deliveryId,
+            orderId = orderDeliveryCreatedEvent.orderId,
+            courierId = UUID.randomUUID().toString()
+        )
+
+        val orderDeliveryPickedUpEvent = OrderDeliveryPickedUpEvent(
+            deliveryId = orderDeliveryCreatedEvent.deliveryId,
+            orderId = orderDeliveryCreatedEvent.orderId,
+            courierId = UUID.randomUUID().toString()
+        )
+
+        val orderDeliveryDeliveredEvent = OrderDeliveryDeliveredEvent(
+            deliveryId = orderDeliveryCreatedEvent.deliveryId,
+            orderId = orderDeliveryCreatedEvent.orderId,
+            courierId = UUID.randomUUID().toString()
+        )
+
+        orderDeliveryHandler.on(orderDeliveryCreatedEvent)
+        orderDeliveryHandler.on(orderDeliveryAcceptedEvent)
+        orderDeliveryHandler.on(orderDeliveryPickedUpEvent)
+        orderDeliveryHandler.on(orderDeliveryDeliveredEvent)
+
+        val savedOrderDelivery =
+            orderDeliveryHandler.handle(FindOrderDeliveryByIdQuery(orderDeliveryAcceptedEvent.deliveryId)).block()
+        savedOrderDelivery shouldNotBe null
+        savedOrderDelivery?.id shouldBe orderDeliveryCreatedEvent.deliveryId
+        savedOrderDelivery?.restaurantLocation shouldBe orderDeliveryCreatedEvent.restaurantLocation
+        savedOrderDelivery?.deliveryLocation shouldBe orderDeliveryCreatedEvent.deliveryLocation
+        savedOrderDelivery?.status shouldBe OrderDeliveryResponse.DeliveryStatus.DELIVERED
+        savedOrderDelivery?.courierFee shouldBe orderDeliveryCreatedEvent.courierFee
+        savedOrderDelivery?.createdAt shouldNotBe null
+        savedOrderDelivery?.courierIdsDeclined?.size shouldBe 0
     }
 }
