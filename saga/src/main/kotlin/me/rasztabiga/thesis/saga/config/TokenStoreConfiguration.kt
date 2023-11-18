c6e7123be40c1f5d4d5dee038a5987b74d99522e
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-11-18 16:20:25 +0100

    feat(backend): WIP fix performance issues

--- saga/src/main/kotlin/me/rasztabiga/thesis/saga/config/TokenStoreConfiguration.kt
+++ saga/src/main/kotlin/me/rasztabiga/thesis/saga/config/TokenStoreConfiguration.kt
@@ -1,12 +1,16 @@
 package me.rasztabiga.thesis.saga.config
 
 import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.model.Indexes
 import org.axonframework.eventhandling.tokenstore.TokenStore
 import org.axonframework.extensions.mongo.DefaultMongoTemplate
 import org.axonframework.extensions.mongo.eventhandling.saga.repository.MongoSagaStore
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore
 import org.axonframework.modelling.saga.repository.SagaStore
 import org.axonframework.serialization.Serializer
+import org.bson.Document
+import org.bson.conversions.Bson
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -30,6 +34,12 @@ class TokenStoreConfiguration {
 
     @Bean
     fun sagaStore(client: MongoClient, serializer: Serializer): SagaStore<Any> {
+        val collection = client.getDatabase("axon-saga").getCollection("sagas")
+        createIndexIfNotExists(collection, Indexes.ascending("sagaIdentifier"))
+        createIndexIfNotExists(collection, Indexes.ascending("sagaType"))
+        createIndexIfNotExists(collection, Indexes.ascending("associations.key"))
+        createIndexIfNotExists(collection, Indexes.ascending("associations.value"))
+
         return MongoSagaStore.builder()
             .mongoTemplate(
                 DefaultMongoTemplate
@@ -39,5 +49,11 @@ class TokenStoreConfiguration {
             )
             .serializer(serializer)
             .build()
+    }
+
+    private fun createIndexIfNotExists(collection: MongoCollection<Document>, index: Bson) {
+        if (!collection.listIndexes().toList().map { it["name"] }.contains(index)) {
+            collection.createIndex(index)
+        }
     }
 }
